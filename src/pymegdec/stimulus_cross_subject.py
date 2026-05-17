c145a6fbfdf9f3dc253f50ce77922a38c13ec705
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-17 18:57:13 +0200

    Load cross-subject chance inference fix

--- src/pymegdec/stimulus_cross_subject.py
+++ src/pymegdec/stimulus_cross_subject.py
@@ -6,6 +6,7 @@ import sys
 
 from pymegdec import _stimulus_cross_subject_core as _core
 from pymegdec import _stimulus_cross_subject_timefix as _timefix  # noqa: F401
+from pymegdec import _stimulus_cross_subject_chance as _chance  # noqa: F401
 
 _impl = _core._impl
 sys.modules[__name__] = _impl
